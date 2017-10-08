4f046219615e41e7eca37b50d1f00acf671d14f2
Michele De Vita <mik3dev@gmail.com>
2017-10-08 19:01:23 +0200

    fixed missing newline and fixed a test problem

--- src/main/scala/json2csv/JsonToCsvImpl.scala
+++ src/main/scala/json2csv/JsonToCsvImpl.scala
@@ -5,18 +5,18 @@ import java.util.Calendar
 class JsonToCsvImpl extends JsonToCsv{
 
   val sep = " ; "
+  val fields: String = List("geometry_type", "geometry_coordinates", "name", "tipo", "typeLabel", "serviceType", "serviceUri", "distance").reduce((a, b) => a + sep + b)
 
   /**
     * Return a csv like the example here: https://github.com/italia/daf-replicate-ingestion/issues/2
     * @param json a json parsed from scala.util.parsing.json.JSON
     * @return
     */
-  val fields = List("geometry_type", "geometry_coordinates", "name", "tipo", "typeLabel", "serviceType", "serviceUri", "distance").reduce((a,b) => a + sep + b)
   override def trasform(json: Map[String, _]): String = {
     val busStopsList = json("BusStops").asInstanceOf[Map[String, _]]("features").asInstanceOf[List[Map[String, _]]]
     val sensorsSitesList = json("SensorSites").asInstanceOf[Map[String, _]]("features").asInstanceOf[List[Map[String, _]]]
     val servicesList = json("Services").asInstanceOf[Map[String, _]]("features").asInstanceOf[List[Map[String, _]]]
-    val timestamp = Calendar.getInstance.getTime.toString
+    val timestamp = "\n" + Calendar.getInstance.getTime.toString
     val queryResultsList = busStopsList ++ sensorsSitesList ++ servicesList
     val csvResults = queryResultsList.map(flatQueryLine)
     return (timestamp +: csvResults).reduce((a,b) => a + "\n" + b)
--- src/tests/scala/TestServicesQuery.scala
+++ src/tests/scala/TestServicesQuery.scala
@@ -9,13 +9,13 @@ class TestServicesQuery extends FlatSpec with Matchers{
   "A Service query" should "produce a well formatted query when passed input params" in{
     val servicesQuery = new ServicesQuery()
     val query = servicesQuery.build(new GPSPoint(0,0), 2, 1)
-    query shouldEqual "http://servicemap.disit.org/WebAppGrafo/api/v1/?format=json&selection=0;0&maxDists=2&maxResults=1"
+    query shouldEqual "http://servicemap.disit.org/WebAppGrafo/api/v1/?format=json&selection=0.0;0.0&maxDists=2&maxResults=1"
   }
 
   it should "produce a well formatted query when not passed input params" in {
     val servicesQuery = new ServicesQuery()
     val query = servicesQuery.build(new GPSPoint(0,0))
-    query shouldEqual "http://servicemap.disit.org/WebAppGrafo/api/v1/?format=json&selection=0;0"
+    query shouldEqual "http://servicemap.disit.org/WebAppGrafo/api/v1/?format=json&selection=0.0;0.0"
   }
 
 }
